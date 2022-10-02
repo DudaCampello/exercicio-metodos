b1abe3f88dac9a1775fe7ade9a2a2eb3ce71cf10
Eduarda <eduarda.campello@hotmail.com>
2022-10-02 11:51:32 -0300

    exemplo calculadora, mensagem e emprestimo

---
+++ src/one/digitalinnivation/basecamp/Main.java
@@ -0,0 +1,25 @@
+package one.digitalinnivation.basecamp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Calculadora
+        System.out.println("Exercicio calculadora: ");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.25);
+
+        //Mensagem
+        System.out.println("Exercicio Mensagem: ");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+
+        //Emprestimo
+        System.out.println("Exercicio Emprestimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+    }
+}
